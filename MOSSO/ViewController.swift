32e0eabdb8d33dcb5b8ffcc2e469ae4e88629a84
Calda <calstephens98@gmail.com>
2014-12-09 09:11:17 -0500

    Working video player

--- MOSSO/ViewController.swift
+++ MOSSO/ViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import Cocoa
+import AVKit
+import AVFoundation
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var playerView: AVPlayerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        var clips : [String] = []
+        
+        let dirs : [String]? = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .AllDomainsMask, true) as? [String]
+        let path = dirs![0].stringByAppendingPathComponent("/MH Instruction/Moment of Silence")
+        let fileManager = NSFileManager.defaultManager()
+        if let enumerator = fileManager.enumeratorAtPath(path) {
+            while let file = enumerator.nextObject() as? String {
+                clips.append(path + "/" + file)
+            }
+        } else {
+            println("Folder not found.")
+        }
+        
+        if clips.count > 0 {
+            playVideoQueue(clips)
+        }
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func playVideoQueue(paths : [String]){
+        println(paths[0])
+        dispatch_async(dispatch_get_main_queue(), {
+            self.playerView.player = AVPlayer(URL: NSURL(fileURLWithPath: paths[0]))
+            self.playerView.player!.play()
+        })
     }
 
     override var representedObject: AnyObject? {
