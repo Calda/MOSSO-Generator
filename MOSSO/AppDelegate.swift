789225946000e060730add4971942dc50922d6d0
Calda <calstephens98@gmail.com>
2014-12-09 08:20:52 -0500

    Initial commit

---
+++ MOSSO/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MOSSO
+//
+//  Created by DFA Film 9: K-9 on 12/9/14.
+//  Copyright (c) 2014 Dog Pound Productions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MOSSO/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MOSSO
+//
+//  Created by DFA Film 9: K-9 on 12/9/14.
+//  Copyright (c) 2014 Dog Pound Productions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
